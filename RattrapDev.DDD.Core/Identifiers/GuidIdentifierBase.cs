c2c924f7ab7138f516b6499731799fea01df96d8
Matt <mateo6379@gmail.com>
2016-08-14 07:52:49 -0500

    Base guid identifier

---
+++ RattrapDev.DDD.Core/Identifiers/GuidIdentifierBase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace RattrapDev.DDD.Core.Identifier
+{
+	public abstract class GuidIdentifierBase : IEquatable<GuidIdentifierBase>
+	{
+		private readonly Guid id;
+
+		protected GuidIdentifierBase(Guid identity)
+		{
+			this.id = identity;
+		}
+
+		protected GuidIdentifierBase()
+		{
+			id = Guid.NewGuid();
+		}
+
+		public Guid Id
+		{
+			get
+			{
+				return this.id;
+			}
+		}
+
+		public bool Equals(GuidIdentifierBase other)
+		{
+			if (ReferenceEquals(this, other)) return true;
+			return other != null && other.Id.Equals(this.Id);
+		}
+	}
+}
+
