6a36a867315db634007ced20ade5bcd4791d5829
Matt <mateo6379@gmail.com>
2016-09-18 14:15:06 -0500

    Quick comments on base contracts

--- RattrapDev.DDD.Core/IDomainEvent.cs
+++ RattrapDev.DDD.Core/IDomainEvent.cs
@@ -2,6 +2,9 @@
 
 namespace RattrapDev.DDD.Core
 {
+	/// <summary>
+	/// Base contract for DDD Domain Events.  
+	/// </summary>
 	public interface IDomainEvent
 	{
 		DateTime OccurredOn { get; }
--- RattrapDev.DDD.Core/IEntity.cs
+++ RattrapDev.DDD.Core/IEntity.cs
@@ -1,5 +1,8 @@
 ﻿namespace RattrapDev.DDD.Core
 {
+	/// <summary>
+	/// Base contract for DDD Entities.
+	/// </summary>
 	public interface IEntity<TIdentifier>
 	{
 		TIdentifier Identifier { get; }
